38dd4941e7d4d16ca8e985ad2f5cef42330f6144
hesamam71 <hesam_am71@yahoo.com>
2026-01-12 12:34:05 +0000

    feat: Add RAM monitoring to system monitor

--- monitor.sh
+++ monitor.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 if [ $(id -u) -ne 0 ]; then
-	echo "Run as admin" && exit 1
+        echo "Run as admin" && exit 1
 fi
-
+ram=$(free -m | awk '/Mem:/ {print $7}')
+if [ $ram -lt 5000 ]; then
+        echo "Critical, ram is low"
+        echo "you have $ram meg space"
+else
+        echo "Ram is healthy"
+        echo "you have $ram meg space"
+fi
 a=$(df -h / | tail -1 | awk '{print $5}' | tr -d '%')
 if [ $a -ge 80 ]; then
-	echo "Critical"
+        echo "Critical"
 else
-	echo "Disk is healthy"
+        echo "Disk is healthy"
 fi
 echo "current disk usage is $a%"
